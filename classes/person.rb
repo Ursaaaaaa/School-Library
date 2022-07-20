e547d27d841ba2fe10bc3c4c81bc32d6626daace
ramrod433 <djoletoamanor@gmail.com>
2022-07-20 00:28:08 +0000

    Added trim and capitalize

--- classes/person.rb
+++ classes/person.rb
@@ -1,17 +1,11 @@
 require_relative './nameable'
 
-
-
 class Person < Nameable
-
   attr_accessor :name, :age
 
   attr_reader :id
 
-
-
   def initialize(age, name = 'Unknown', parent_permission: true)
-
     @id = rand(1..10_000)
 
     @name = name
@@ -19,36 +13,20 @@ class Person < Nameable
     @age = age
 
     @parent_permission = parent_permission
-
+    super()
   end
-
-
 
   def can_use_services?
-
     of_age? || @parent_permission
-
   end
-
-
 
   def correct_name
-
     @name
-
   end
-
-
 
   private
 
-
-
   def of_age?
-
     @age >= 18
-
   end
-
 end
-
