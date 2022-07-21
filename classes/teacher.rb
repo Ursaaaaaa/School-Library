8ff70a0fbbee8d70538d2dcbb4c05c13eab67301
ramrod433 <djoletoamanor@gmail.com>
2022-07-21 16:19:53 +0000

    fix linters

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,13 +1,26 @@
 require_relative './person'
 
+
+
 class Teacher < Person
+
   def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+
     super(age, parent_permission: parent_permission)
+
     @specialization = specialization
+
     @name = name
+
   end
 
+
+
   def can_use_services?
+
     true
+
   end
+
 end
+
